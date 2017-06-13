3abd5bf96fadbbe1644323e2317a00202a8f9d99
Oliver Schulz <oschulz@mpp.mpg.de>
2017-06-13 17:22:35 +0200

    Make macro everyworker Julia v0.6 compatible

--- src/processexec.jl
+++ src/processexec.jl
@@ -3,10 +3,19 @@
 
 export @everyworker
 macro everyworker(expr)
-    expr = Base.localize_vars(esc(:(()->($expr))), false)
-    i = gensym("idx")
-    quote
-        futures = map($i -> Base.spawnat($i, $expr), workers())
-        fetch.(futures)
+    @static if VERSION < v"0.6.0-"
+        expr = Base.localize_vars(esc(:(()->($expr))), false)
+        i = gensym("idx")
+        quote
+            futures = map($i -> Base.spawnat($i, $expr), workers())
+            fetch.(futures)
+        end
+    else
+        expr = esc(:(()->($expr)))
+        i = gensym("idx")
+        quote
+            futures = map($i -> Base.Distributed.spawnat($i, $expr), workers())
+            fetch.(futures)
+        end
     end
 end
