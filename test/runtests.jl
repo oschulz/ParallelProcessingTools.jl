8d84b6c0520bc380c62a7eec82d3cdb1ffd99dd0
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-22 10:03:21 +0200

    Require Julia v0.6, update CI configs, prepare for Julia v0.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 # This file is a part of MultiThreadingTools.jl, licensed under the MIT License (MIT).
 
-@Base.Test.testset "Package MultiThreadingTools" begin
+import Compat.Test
+Test.@testset "Package MultiThreadingTools" begin
     include.([
     ])
 end
