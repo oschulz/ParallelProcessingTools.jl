2313efb72fc70e930be68f2f4b50aded8c260b24
Oliver Schulz <oschulz@mpp.mpg.de>
2019-08-11 23:41:11 +0200

    Add Documenter docs

---
+++ docs/make.jl
@@ -0,0 +1,30 @@
+# Use
+#
+#     DOCUMENTER_DEBUG=true julia --color=yes make.jl local [nonstrict] [fixdoctests]
+#
+# for local builds.
+
+using Documenter
+using ParallelProcessingTools
+
+makedocs(
+    sitename = "ParallelProcessingTools",
+    modules = [ParallelProcessingTools],
+    format = Documenter.HTML(
+        prettyurls = !("local" in ARGS),
+        canonical = "https://oschulz.github.io/ParallelProcessingTools.jl/stable/"
+    ),
+    pages=[
+        "Home" => "index.md",
+        "API" => "api.md",
+        "LICENSE" => "LICENSE.md",
+    ],
+    doctest = ("fixdoctests" in ARGS) ? :fix : true,
+    linkcheck = ("linkcheck" in ARGS),
+    strict = !("nonstrict" in ARGS),
+)
+
+deploydocs(
+    repo = "github.com/oschulz/ParallelProcessingTools.jl.git",
+    forcepush = true
+)
---
+++ docs/src/api.md
@@ -0,0 +1,26 @@
+# API
+
+```@meta
+DocTestSetup  = quote
+    using ParallelProcessingTools
+end
+```
+
+## Types
+
+```@index
+Order = [:type]
+```
+
+## Functions and macros
+
+```@index
+Order = [:macro, :function]
+```
+
+# Documentation
+
+```@autodocs
+Modules = [ParallelProcessingTools]
+Order = [:type, :macro, :function]
+```
