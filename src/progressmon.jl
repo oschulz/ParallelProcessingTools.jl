cb669ae58e2e22276f3c450bc7f840305aa36d36
Oliver Schulz <oschulz@mpp.mpg.de>
2022-04-10 01:09:30 +0200

    STASH

--- src/progressmon.jl
+++ src/progressmon.jl
@@ -4,8 +4,15 @@
 struct ProgressState
     from::Float64
     to::Float64
-    current::Float64
+    t_start::UInt64
+    progress::Float64
+    t_current::UInt64
 end
+
+ProgressState() = ProgressState(0, 1, time_ns(), 0)
+
+ProgressState(old::ProgressState, progress::Float64, t_current::UInt64) =
+    ProgressState(old.from, old.to, old.t_start, progress, t_current)
 
 
 mutable struct ProgressEntry
@@ -17,7 +24,7 @@ end
 
 
 #!!!! make const
-g_progress_channel_lock = ReentrantLock()
+g_progress_lock = ReentrantLock()
 
 #!!!! use const Ref
 g_progress_channel = nothing
@@ -25,20 +32,52 @@ g_progress_channel = nothing
 #!!!! make const
 g_progress_states = IdDict{Int,ProgressEntry}()
 
+#!!!! make const
+g_progress_nextid = Atomic{Int}(0)
+
+
+function _progresss_channel_impl(ch::Channel{Tuple{Int,Float64,Bool}})
+    while true
+        try
+            lock(g_progress_lock)
+            if isempty(g_progress_states)
+                break
+            else
+                id, progress, done = take!(ch)
+                entry = g_progress_states[id]
+                ProgressState(entry.state, progress, t_current)
+                new_entry = ProgressEntry(entry.from, entry.to, progress)
+                g_progress_states[id] = new_entry
+
+                #!!!!! show progress
+                @info "Total progress:"
+            end
+        finally
+            unlock(g_progress_lock)
+        end
+    end
+end
 
 function _register_progress_impl(description::AbstractString, state::ProgressState, parent::Union{Int,Nothing})
     try
-        lock(g_progress_channel_lock)
-        if isnothing(g_progress_channel)
-            g_progress_channel = Channel(_progresss_channel_impl, spawn = true)
+        lock(g_progress_lock)
+        id = atomic_add!(g_progress_nextid, 1)
+        entry = ProgressEntry(id, parent, description, state)
+        if isnothing(g_progress_channel) || !isopen(g_progress_channel)
+            @assert isempty(g_progress_states)
+            g_progress_states[id] = entry
+            g_progress_channel = Channel(_progresss_channel_impl, 1000, spawn = true)
+        else
+            @assert !haskey(g_progress_states, id)
+            g_progress_states[id] = entry
         end
-        g_progress_channel
+        id, g_progress_channel
     finally
-        unlock(g_progress_channel_lock)
+        unlock(g_progress_lock)
     end
 end
 
 
-function register_progress(description::AbstractString, state::ProgressState = ProgressState(0, 0, 0) ; parent::Union{Int,Nothing} = nothing)
+function register_progress(description::AbstractString, state::ProgressState = ProgressState() ; parent::Union{Int,Nothing} = nothing)
     id, channel = remotecall_fetch(_register_progress_impl, 1, description, state, parent)
 end
