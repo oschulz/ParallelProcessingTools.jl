4897aeafc98a244e5ea597d629dbd15c29a5b4b9
Oliver Schulz <oschulz@mpp.mpg.de>
2024-04-30 10:48:29 +0200

    Adds inner_exception, original_exception get's a new role

--- src/exceptions.jl
+++ src/exceptions.jl
@@ -1,17 +1,34 @@
 # This file is a part of ParallelProcessingTools.jl, licensed under the MIT License (MIT).
 
 """
+    ParallelProcessingTools.inner_exception(err)
+
+Replaces exceptions like a `TaskFailedException` or a `RemoteException` with
+their underlying cause. Leaves other exceptions unchanged.
+"""
+function inner_exception end
+export inner_exception
+
+inner_exception(err) = err
+inner_exception(err::CompositeException) = CompositeException(inner_exception.(err.exceptions))
+inner_exception(err::TaskFailedException) = err.task.result
+inner_exception(err::RemoteException) = err.captured.ex
+
+
+"""
     ParallelProcessingTools.original_exception(err)
 
-Replaces `TaskFailedException`s and `RemoteException`s with the underlying
-exception that originated within the task or on the remote process.
+Replaces (possibly nested) exceptions like a `TaskFailedException` or
+`RemoteException`s with the innermost exception, likely to be the one that
+was thrown originally. Leaves other exceptions unchanged.
 """
 function original_exception end
+export original_exception
 
 original_exception(err) = err
 original_exception(err::CompositeException) = CompositeException(original_exception.(err.exceptions))
-original_exception(err::TaskFailedException) = err.task.result
-original_exception(err::RemoteException) = err.captured.ex
+original_exception(err::TaskFailedException) = original_exception(err.task.result)
+original_exception(err::RemoteException) = original_exception(err.captured.ex)
 
 
 """
@@ -19,12 +36,13 @@ original_exception(err::RemoteException) = err.captured.ex
 
 Replaces `CompositeException`s with their first exception.
 
-Also employs `original_exception` if `simplify` is `true`.
+Also employs `inner_exception` if `simplify` is `true`.
 """
 function onlyfirst_exception end
+export onlyfirst_exception
 
 onlyfirst_exception(err) = err
-onlyfirst_exception(err::CompositeException) = first(err.exceptions)
+onlyfirst_exception(err::CompositeException) = first(err)
 
 
 """
@@ -36,14 +54,14 @@ If multiple exceptions originate from parallel code in `expr`, only one
 is rethrown, and `TaskFailedException`s and `RemoteException`s are replaced
 by the original exceptions that caused them.
 
-See [`original_exception`] and [`onlyfirst_exception`](@ref).
+See [`inner_exception`] and [`onlyfirst_exception`](@ref).
 """
 macro userfriendly_exceptions(expr)
     quote
         try
             $(esc(expr))
         catch err
-            rethrow(original_exception(onlyfirst_exception(err)))
+            rethrow(inner_exception(onlyfirst_exception(err)))
         end
     end
 end
