407fc6dd60b8c37e98ad859ac2261d27448da581
Oliver Schulz <oschulz@mpp.mpg.de>
2026-07-10 15:00:41 +0200

    Fix read! on LockableIO forwarding to read instead of read!
    Created by generative AI.

--- src/threadsafe.jl
+++ src/threadsafe.jl
@@ -53,7 +53,7 @@ LockableIO(x::T) where {T<:IO} = LockableIO{T}(x)
 end
 
 @inline Base.read!(lio::LockableIO, args...; kwargs...) = map(lio) do io
-    read(io, args...; kwargs...)
+    read!(io, args...; kwargs...)
 end
 
 @inline Base.write(lio::LockableIO, args...; kwargs...) = map(lio) do io
